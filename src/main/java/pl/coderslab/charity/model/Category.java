7694c974a4c7a1196a9212c7b9e4c1669734ab31
Michał Szyba <m.szyba@gmail.com>
2021-06-14 23:12:14 +0200

    added Donation model, repository, service

--- src/main/java/pl/coderslab/charity/model/Category.java
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
@@ -16,4 +17,7 @@ public class Category {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    List<Donation> donations;
 }
---
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -0,0 +1,35 @@
+package pl.coderslab.charity.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "donations")
+@Getter @Setter @NoArgsConstructor
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToMany
+    private List<Category> categories;
+
+    @ManyToOne
+    private Institution institution;
+
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+}
