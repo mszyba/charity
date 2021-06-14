30794a5fc780b54c7a2be4ce749fe841a65bc733
Michał Szyba <m.szyba@gmail.com>
2021-06-14 22:51:37 +0200

    added Category model, repository, service

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Getter @Setter @NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
