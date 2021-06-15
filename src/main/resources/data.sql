6330adba72c47c0efbb73cc8c28c5d68613f3e9c
Michał Szyba <m.szyba@gmail.com>
2021-06-15 20:44:34 +0200

    added random data to .sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO institutions (description, name) VALUES ('Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.', 'Fundacja "Dla dzieci"');
+INSERT INTO institutions (description, name) VALUES ('desc2', 'Fundacja 2"');
+INSERT INTO institutions (description, name) VALUES ('desc3', 'Fundacja 3"');
+INSERT INTO institutions (description, name) VALUES ('desc4', 'Fundacja 34');
