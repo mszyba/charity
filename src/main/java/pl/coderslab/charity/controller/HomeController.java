bafabe640281ef94dbc7e3294a4a51250d3b1785
Michał Szyba <m.szyba@gmail.com>
2021-06-15 22:29:33 +0200

    #2 home - list of institute

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -2,10 +2,26 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.service.InstitutionService;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private final InstitutionService institutionService;
+
+    public HomeController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
+
+    @ModelAttribute("institutions")
+    public List<Institution> listInstitution() {
+        return institutionService.getAll();
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
-INSERT INTO institutions (description, name) VALUES ('Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.', 'Fundacja "Dla dzieci"');
-INSERT INTO institutions (description, name) VALUES ('desc2', 'Fundacja 2"');
-INSERT INTO institutions (description, name) VALUES ('desc3', 'Fundacja 3"');
-INSERT INTO institutions (description, name) VALUES ('desc4', 'Fundacja 34');
+INSERT INTO institutions (description, name) VALUES ('desc1', 'Fundacja 1');
+INSERT INTO institutions (description, name) VALUES ('desc2', 'Fundacja 2');
+INSERT INTO institutions (description, name) VALUES ('desc3', 'Fundacja 3');
+INSERT INTO institutions (description, name) VALUES ('desc4', 'Fundacja 4');
+INSERT INTO institutions (description, name) VALUES ('desc5', 'Fundacja 5');
+INSERT INTO institutions (description, name) VALUES ('desc6', 'Fundacja 6');
