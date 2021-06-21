3e3255e2ea920f8bc07c5e014f742d3ea0af7c50
Michał Szyba <m.szyba@gmail.com>
2021-06-21 23:36:16 +0200

    added sb-admin-2

---
+++ src/main/java/pl/coderslab/charity/controller/AdminController.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.service.UserService;
+
+@Controller
+public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/admin/login")
+    public String adminLoginForm() {
+        return "/admin/admin-login";
+    }
+}
