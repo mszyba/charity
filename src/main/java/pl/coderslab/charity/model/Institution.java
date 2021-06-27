ca2117772e900ddacfefaf65cd5cd6caa53d8507
Michał Szyba <m.szyba@gmail.com>
2021-06-27 20:48:49 +0200

    added Set donations to Institution

--- src/main/java/pl/coderslab/charity/model/Institution.java
+++ src/main/java/pl/coderslab/charity/model/Institution.java
@@ -3,13 +3,16 @@ package pl.coderslab.charity.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Entity
 @Table(name = "institutions")
 @Getter @Setter @NoArgsConstructor
+@ToString
 public class Institution {
 
     @Id
@@ -21,4 +24,7 @@ public class Institution {
 
     @Size(min = 3, max = 255, message = "Minimum 3 znaki")
     private String description;
+
+    @OneToMany(mappedBy = "institution", cascade = CascadeType.ALL)
+    private Set<Donation> donations;
 }
